2eb045ddc7a2d89647b41e84138731b7b85a0e34
Masen <77242074+masen-f@users.noreply.github.com>
2022-04-10 14:03:39 -0400

    Add files via upload

---
+++ install_linux/install_deb.sh
@@ -0,0 +1,12 @@
+echo "Installing dependencies..."
+
+sudo apt install python-opencv
+sudo apt install xscreensaver
+pip install sklearn
+pip install imutils
+pip install numpy
+pip install h5py
+pip install dlib
+pip install face-recognition
+
+echo "Finished installing dependencies"
